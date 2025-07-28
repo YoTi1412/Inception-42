9fc8c27f8b76a04ff35133186c17826fe6e6c686
YoTi <71252194+YoTi1412@users.noreply.github.com>
2025-07-28 13:03:51 +0100

    static page is working fine

--- requirements/nginx/tools/entrypoint.sh
+++ requirements/nginx/tools/entrypoint.sh
@@ -4,8 +4,7 @@ if [ ! -f /etc/nginx/ssl/yoti.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/yoti.key \
         -out /etc/nginx/ssl/yoti.crt \
-        -subj "/CN=yoti.42.fr"
+        -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=yoti.42.fr"
 fi
 
-exec nginx -g 'daemon off;'
-
+exec nginx -g "daemon off;"
