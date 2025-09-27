add5e76392cda7019cd5e433fc46fd898763c492
yasser rafai <yasserrafai5@gmail.com>
2025-09-28 00:33:06 +0100

    ALL DONE

--- srcs/requirements/bonus/portainer/tools/portainer-entrypoint.sh
+++ srcs/requirements/bonus/portainer/tools/portainer-entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-PORTAINER_VERSION=2.27.9
 PORTAINER_URL="https://github.com/portainer/portainer/releases/download/${PORTAINER_VERSION}/portainer-${PORTAINER_VERSION}-linux-amd64.tar.gz"
 
-echo "📦 Downloading Portainer CE ${PORTAINER_VERSION}..."
-mkdir -p /app
+mkdir -p /app/portainer
+curl -L "$PORTAINER_URL" -o /app/portainer/portainer.tar.gz
+tar -xzf /app/portainer/portainer.tar.gz -C /app/portainer --strip-components=1
+rm /app/portainer/portainer.tar.gz
+chmod +x /app/portainer/portainer
 
-curl -L "$PORTAINER_URL" -o /app/portainer.tar.gz
-
-echo "📂 Extracting..."
-tar -xzf /app/portainer.tar.gz -C /app
-
-rm /app/portainer.tar.gz
-
-echo "✅ Starting Portainer..."
-exec /app/portainer/portainer
+exec /app/portainer/portainer \
+     --data /data \
+     --admin-password-file /run/secrets/portainer_password
